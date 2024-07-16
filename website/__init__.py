aeb4820b3232e3aebd76504406dc2aa5957ad19c
João Rosado <91827990+Rosacio@users.noreply.github.com>
2024-07-16 01:17:31 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SECRET_KEY'] = 'omeuprojetofinal'
 
     from .views import views
     from .auth import auth
