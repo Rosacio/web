17f6e341ef3fb0bd32455a1fc8bef2b6f4ab0df3
João Rosado <91827990+Rosacio@users.noreply.github.com>
2024-07-16 11:42:59 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,8 +5,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'omeuprojetofinal'
 
     from .views import views
-    from .auth import auth
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth/')
 
     return app
